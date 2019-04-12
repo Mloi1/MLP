bddf601d130940749a10afc49e8e23cb5c643ef1
Marti <martina.loi024@gmail.com>
2019-04-12 12:27:16 +0100

    completion of mobile version

--- scripts/formFunctions.js
+++ scripts/formFunctions.js
@@ -10,52 +10,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const items = {
         bosa: {
             price: 19.99,
-            size: "50x70",
         },
         geyser: {
             price: 19.99,
-            size: "50x70",
         },
         glacier: {
             price: 15.99,
-            size: "50x85",
-
         },
         porto: {
             price: 20.99,
-            size: "50x120",
         },
-        redSquirrel: {
+        squirrel: {
             price: 20.99,
-            size: "70x50",
-        },
-        rainyLondon: {
-            price: 20.99,
-            size: "50x65",
-        },
-        murales: {
-            price: 19.99,
-            size: "50x70",
-        },
-        tightropeWalkers: {
-            price: 19.99,
-            size: "50x67",
-        },
-        iceland: {
-            price: 20.99,
-            size: "50x65",
         },
         stPaul: {
             price: 20.99,
-            size: "50x70",
+        },
+        murales: {
+            price: 19.99,
+        },
+        tavira: {
+            price: 19.99,
+        }, 
+        ice: {
+            price: 20.99,
+        },
+        reflection: {
+            price: 20.99,
         },
         towerBridge: {
             price: 20.99,
-            size: "50x65",
         },
-        peaceAndQuiet: {
+        peace: {
             price: 20.99,
-            size: "50x65",
         }
     }
 
@@ -66,7 +53,6 @@ document.addEventListener("DOMContentLoaded", () => {
             const itemType = entry[0]
             const quantity = entry[1]
             const unitPrice = items[itemType].price
-            const imgSrc = items[itemType].img
             const subTotal = unitPrice * quantity
             total += subTotal
             const newDiv = document.createElement("div")
@@ -75,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
             cart.appendChild(newDiv)
         })
         const newTotal = document.createElement("div");
-        newDiv.classList.add("item-row")
+        newTotal.classList.add("item-row")
         newTotal.innerHTML = total ? `<span class="grand-total">Total: ${total}</span>` : 'No Items Purchased Yet'
         cart.appendChild(newTotal)
     }
@@ -93,4 +79,3 @@ document.addEventListener("DOMContentLoaded", () => {
     addToCart.addEventListener("click", addToCartHandler)
 })
 
-s
